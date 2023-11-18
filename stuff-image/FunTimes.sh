082793e258dda042fac61091d8a2f4245fbf4fef
sbunsnubs <decemstix@gmail.com>
2023-11-17 18:26:41 -1000

    d

--- stuff-image/FunTimes.sh
+++ stuff-image/FunTimes.sh
@@ -3,11 +3,13 @@
 #!/bin/bash
 
 #sudo apt -y install docker.io
+#sudo systemctl enable docker.server
+#sudo systemctl anable containerd.service
 #sudo systemctl start docker
 #docker --version 
 #verification that docker was installed
 
-docker container build -t FunTimes .
+docker container build -it FunTimes .
 #building container
 docker run FunTimes 
 #running container with packages
