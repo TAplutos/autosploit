82a965a5f8f4d76533ca2f80cead0fa102707368
TAplutos <TA.plutos@gmail.com>
2023-11-23 10:38:26 +0900

    added more vulns

--- knownVulnerabilities.py
+++ knownVulnerabilities.py
@@ -208,4 +208,4 @@ _postgresDict = Vulnerability(_postgresDict)
 
 # removed so this doesnt take 8 years each time when testing
 # _smtpScanner
-vulnerabilities = [_ircd, _distcc, _vsftpd, _tomcatDict, _sambaDict, _apacheDict, _postgresDict]
+vulnerabilities = [_ircd, _distcc, _vsftpd, _smtpScanner, _tomcatDict, _sambaDict, _apacheDict, _postgresDict]
